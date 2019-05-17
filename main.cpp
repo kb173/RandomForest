08aafc9c2fb450807f772fcf2d8331f53fae457d
Karl Bittner <karl.bittner@outlook.com>
2019-05-17 18:08:48 +0200

    Add datasets and mnist reader from wichtounet

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "mnist_reader.hpp"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+    // Load MNIST data
+    mnist::MNIST_dataset<std::vector, std::vector<uint8_t>, uint8_t> dataset =
+            mnist::read_dataset<std::vector, std::vector, uint8_t, uint8_t>("/home/karl/Data/Technikum/SEM4/MLE/RandomForest/Datasets");
+
+    std::cout << "Nbr of training images = " << dataset.training_images.size() << std::endl;
+    std::cout << "Nbr of training labels = " << dataset.training_labels.size() << std::endl;
+    std::cout << "Nbr of test images = " << dataset.test_images.size() << std::endl;
+    std::cout << "Nbr of test labels = " << dataset.test_labels.size() << std::endl;
+
     return 0;
 }
